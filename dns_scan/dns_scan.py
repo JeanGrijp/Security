6fe0cedb0308a34428fc09f88488d174461a6e30
Jean Grijp <40359722+grijpjean@users.noreply.github.com>
2019-05-19 02:00:02 -0300

    Add files via upload

--- dns_scan/dns_scan.py
+++ dns_scan/dns_scan.py
@@ -2,17 +2,19 @@ import dns.resolver
 import sys
 
 
-dominio = sys.argv[1]
-nome_arquivo = sys.argv[2]
-arquivo = open(nome_arquivo,'r')
+#dominio = sys.argv[1]
+#nome_arquivo = sys.argv[2]
+#arquivo = open(nome_arquivo,'r')
+arquivo = open("dns.txt",'r')
+dominio = "globo.com"
 subdominiuns = arquivo.readlines()
 for i in subdominiuns:
     sub = i[:len(i)-1]
     dns_nome = sub + "." + dominio
     try:
-        resultado = dns.resolver.query(dns_nome)
+        resultado = dns.resolver.query(qname=dns_nome, tcp=False, rdtype="a")
         for resposta in resultado:
-            print(dns_nome, resultado[0].address)
+            print("SUBDOMINIO: ",sub,"  /  " "DNS: ", dns_nome,"  /  IP: ", resultado[0].address)
     except:
         pass
 arquivo.close()
