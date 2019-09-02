374592aef73da48c1c06b4b973e0055f2f515b9b
Jean Grijp <grijpjean@gmail.com>
2019-09-01 22:00:10 -0300

    Mudando arquivos de lugar

---
+++ Vulnerabilidades Web/dns_scan/dns_scan.py
@@ -0,0 +1,18 @@
+import dns.resolver
+import sys
+
+
+dominio = sys.argv[1]
+nome_arquivo = sys.argv[2]
+arquivo = open(nome_arquivo,'r')
+subdominiuns = arquivo.readlines()
+for i in subdominiuns:
+    sub = i[:len(i)-1]
+    dns_nome = sub + "." + dominio
+    try:
+        resultado = dns.resolver.query(qname=dns_nome, tcp=False, rdtype="a")
+        for resposta in resultado:
+            print("SUBDOMINIO: ",sub,"  /  " "DNS: ", dns_nome,"  /  IP: ", resultado[0].address)
+    except:
+        pass
+arquivo.close()
